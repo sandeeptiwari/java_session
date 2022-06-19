320c5e842c1357a682762f708e3208dae0b87c09
Poonam <107668462+poonamgit01@users.noreply.github.com>
2022-06-19 18:41:12 +0530

    Assignment1

---
+++ Assignment1.java
@@ -0,0 +1,20 @@
+package tranning;
+import java.util.Scanner;
+public class Assignment1 {
+
+	public static void main(String args[])
+	{
+	int i,j,row;
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the No of Row to Print Pattern");
+	row = sc.nextInt();
+	for(i=0; i<row; i++)
+	{
+	for(j=0; j<=i; j++)
+	{
+	System.out.print("* ");
+	}
+	System.out.println();
+	}
+	}
+	}
