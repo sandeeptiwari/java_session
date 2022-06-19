83a488725251303d1bc20f88cd2c7c3ea361d4d1
DhruvDabur07 <107610262+DhruvDabur07@users.noreply.github.com>
2022-06-19 19:56:48 +0530

    Add files via upload

---
+++ DhruvDaburAssignments/Assignment 1/Test.java
@@ -0,0 +1,8 @@
+package assignments;
+
+public interface Test {
+
+	final static int x = 10;
+
+	public void print();
+}
---
+++ DhruvDaburAssignments/Assignment 1/TestInheritance.java
@@ -0,0 +1,14 @@
+package assignments;
+
+public class TestInheritance implements Test {
+
+	final static int y;
+	static {
+		y = 10;
+	}
+
+	public void print() {
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
