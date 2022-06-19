8ce392eabe68456ea5b0f5881e6f9cdf0c35afec
ashutoshaudax <107641562+ashutoshaudax@users.noreply.github.com>
2022-06-19 13:07:40 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,40 @@
+package Day4;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		int i,j,n;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows you want to print : ");
+		n = sc.nextInt();
+		int space = n-1;
+		
+		for (j=1; j<=n; j++) {
+			for (i = 1; i<= space; i++) {
+				System.out.print(" ");
+			}
+			space--;
+			
+			for (i = 1; i <= 2 * j - 1; i++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+			
+			
+		}
+		space = 1;
+		for (j = 1; j<= n - 1; j++) {
+			for (i = 1; i<= space; i++) {
+				System.out.print(" ");
+			}
+			space++; 
+			for (i = 1; i<= 2 * (n - j) - 1; i++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+
+}
---
+++ pattern4.java
@@ -0,0 +1,28 @@
+package Day4;
+
+import java.util.Scanner;
+
+public class pattern4 {
+
+	public static void main(String[] args) {
+		
+		int i,j,k,row;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the No of Row to Print Pattern");
+		row = sc.nextInt();
+
+		for (i= 0; i<= row-1; i++)
+		{
+		for (j=0; j<=i; j++)
+		{
+		System.out.print(" ");
+		}
+		for (k=0; k<=row-1-i; k++)
+		{
+		System.out.print("*" + " ");
+		}
+		System.out.println();
+		}
+	}
+
+}
